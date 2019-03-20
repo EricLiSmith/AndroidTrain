51abc272e5b5ebf30522db0cc04e17ad0f6d89c8
DuDuMB\Eric <ericleetask@gmail.com>
2019-03-20 23:32:45 +0800

    add CheckBox

--- PassToPhone/app/src/main/java/com/dudu/passtophone/MainActivity.kt
+++ PassToPhone/app/src/main/java/com/dudu/passtophone/MainActivity.kt
@@ -6,13 +6,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), TextView.OnEditorActionListener {
     private val logTAG = "MainActivity"
     private lateinit var etPhoneNumber: EditText
+
+    private var checkedItems = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,5 +57,13 @@ class MainActivity : AppCompatActivity(), TextView.OnEditorActionListener {
         } else {
             Log.d(logTAG, "Can't handle this!")
         }
+    }
+
+    fun addCheckItem(view: View){
+        checkedItems += (view as CheckBox).text.toString()
+    }
+
+    fun showCheckedItems(@Suppress("UNUSED_PARAMETER")view: View){
+        Toast.makeText(this, checkedItems, Toast.LENGTH_SHORT).show()
     }
 }
